f47494c1d6271253b2d165be3f95d40f13ffa735
Arpita Aparna <arpita.aparna04@gmail.com>
2025-08-16 21:45:41 +0530

    Made some minor changes in page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      K-1000 Manager Project
+    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-t from-black to-blue-950 ">
+      <div className="flex flex-center">
+        <h1 className="text-8xl justify-start text-gray-700 z-50 shadow-lg font-bold">K1000</h1>
+      </div> 
     </div>
   );
 }
